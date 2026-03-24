7256e5f7cd2a6c4fb1b7d4484c62284973699bb4
Nazrul Islam <nazrulislambhat@gmail.com>
2026-03-24 09:57:33 +0530

    added salah tracker & masjid finder

--- components/NotificationSettings.tsx
+++ components/NotificationSettings.tsx
@@ -81,7 +81,7 @@ function Toggle({
       }`}
     >
       <span
-        className={`absolute top-0.5 h-5 w-5 rounded-full bg-white shadow transition-transform ${
+        className={`absolute left-0 top-0.5 h-5 w-5 rounded-full bg-white shadow transition-transform ${
           enabled ? 'translate-x-5' : 'translate-x-0.5'
         }`}
       />
